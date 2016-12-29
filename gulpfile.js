d30ef882b59ec48adc3e47bcea8bf0924353614d
Raimov Rinat <raimovrinat@yandex.ru>
2016-12-30 00:50:19 +0300

    базовая разметка файла index.html

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,7 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var mqpacker = require("css-mqpacker");
-var minify = require("gulp-csso");
-var imagemin = require("gulp-imagemin");
-var rename = require("gulp-rename");
-var svgstore = require("gulp-svgstore");
-var svgmin = require("gulp-svgmin");
-var del = require("del");
-var run = require("run-sequence");
 var server = require("browser-sync").create();
-
-gulp.task("images", function() {
-  return gulp.src("build/img/**/*.{png, jpg,gif}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
-      ]))
-    .pipe(gulp.dest("build/img"))
-});
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -30,63 +13,16 @@ gulp.task("style", function() {
     .pipe(less())
     .pipe(postcss([
       autoprefixer({browsers: [
-        "last 1 versions",
-        "last 2 Chrome versions",
-        "last 2 Firefox versions",
-        "last 2 Opera versions",
-        "last 2 Edge versions"
-      ]}),
-      mqpacker({
-        sort: true
-      })
+        "last 2 versions"
+      ]})
     ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"));
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
 });
 
-gulp.task("symbols", function() {
-  return gulp.src("build/img/icons/*.svg")
-    .pipe(svgmin())
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("symbols.svg"))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("copy", function() {
-  return gulp.src([
-    "fonts/**/*.{woff,woff2}",
-    "img/**",
-    "js/**",
-    "*.html"
-  ], {
-    base: "."
-})
-  .pipe(gulp.dest("build"));
-});
-
-
-gulp.task("clean", function() {
-  return del("build");
-});
-
-gulp.task("build", function(fn) {
-  run(
-    "clean",
-    "copy",
-    "style",
-    "images",
-    "symbols",
-    fn
-  );
-});
-
-gulp.task("serve", function() {
+gulp.task("serve", ["style"], function() {
   server.init({
-    server: "build",
+    server: ".",
     notify: false,
     open: true,
     cors: true,
